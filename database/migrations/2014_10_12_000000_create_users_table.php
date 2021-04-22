64122cd98ae4daea0ec89872fa1a006185ab744d
charnmongkol <79741950+charnmongkol@users.noreply.github.com>
2021-04-22 17:37:50 +0700

    second comment

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('role')->default('user')->comment('admin / user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', HomeComponent::class);
+
+Route::get('/shop', ShopComponent::class);
+
+Route::get('/cart', CartComponent::class);
+
+Route::get('/checkout', CheckoutComponent::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+//For Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
+
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin-dashboard');
+});
+
+//For User
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user-dashboard');
 });
